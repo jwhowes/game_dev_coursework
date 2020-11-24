ac9563189aa3b47afa96ca5e7501cf938c5076bc
jwhowes <jacob.w.howes@durham.ac.uk>
2020-11-24 18:14:19 +0000

    Added life time to health drops

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPickup : MonoBehaviour
-{
+public class HealthPickup : MonoBehaviour{
+    public float lifeTime;
     void OnTriggerEnter(Collider other){
         if(other.tag == "Player"){
             if (other.GetComponent<PlayerHealth>().Heal(10)){
                 Destroy(gameObject);
             }
+        }
+    }
+
+    void Update(){
+        lifeTime -= Time.deltaTime;
+        if(lifeTime <= 0){
+            Destroy(gameObject);
         }
     }
 }
