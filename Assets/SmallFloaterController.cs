52d29c60bbb8c59859d111b1171d95094b778e17
jwhowes <jacob.w.howes@durham.ac.uk>
2020-11-28 18:08:03 +0000

    Imported some textures and started working on small floaters

---
+++ Assets/SmallFloaterController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallFloaterController : MonoBehaviour{
+    public Rigidbody rb;
+    public float speed;
+    void OnTriggerStay(Collider other){
+        rb.AddForce((transform.position - other.ClosestPoint(transform.position)).normalized * speed, ForceMode.Acceleration);
+    }
+}
