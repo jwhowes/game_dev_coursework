c4b09fa47492640c6563f516f33c32ccec0f1efe
jwhowes <jacob.w.howes@durham.ac.uk>
2020-11-30 20:16:29 +0000

    Began working on main menu

--- Assets/Scripts/FloaterShoot.cs
+++ Assets/Scripts/FloaterShoot.cs
@@ -7,15 +7,26 @@ public class FloaterShoot : EnemyShootBomb{
     public float subFireRate;
     public int subProjectiles;
 
+    public float spawnMaxTimer;
+    public float spawnMinTimer;
+    public GameObject smallFloater;
+
+    private float spawnTimer;
     private float mainCountdown = 0;
     private float subCountdown = 0;
     private bool firingSub = false;
     private int subFired = 0;
     void Update(){
-        if(CanSeeTarget() && mainCountdown <= 0){
-            firingSub = true;
-            subFired = 0;
-            mainCountdown = mainFireRate;
+        if(CanSeeTarget()){
+            if(mainCountdown <= 0){
+                firingSub = true;
+                subFired = 0;
+                mainCountdown = mainFireRate;
+            }
+            if(spawnTimer <= 0){
+                Instantiate(smallFloater, transform.position + (new Vector3(Random.value, Random.value, Random.value)).normalized, Quaternion.identity);
+                spawnTimer = Random.Range(spawnMinTimer, spawnMaxTimer);
+            }
         }
         if(firingSub && subFired < subProjectiles && subCountdown <= 0){
             subCountdown = subFireRate;
@@ -27,5 +38,6 @@ public class FloaterShoot : EnemyShootBomb{
         }
         mainCountdown -= Time.deltaTime;
         subCountdown -= Time.deltaTime;
+        spawnTimer -= Time.deltaTime;
     }
 }
--- Assets/SmallFloaterController.cs
+++ Assets/SmallFloaterController.cs
@@ -5,7 +5,41 @@ using UnityEngine;
 public class SmallFloaterController : MonoBehaviour{
     public Rigidbody rb;
     public float speed;
+    public float lifeSpan;
+    public float blastRadius;
+    public float baseDamage;
+    public GameObject target;
+    public GameObject explosionEffect;
+
+    private float age;
+    void Start(){
+        target = PlayerManager.instance.player;
+        age = lifeSpan;
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            Explode();
+        }
+    }
+
     void OnTriggerStay(Collider other){
         rb.AddForce((transform.position - other.ClosestPoint(transform.position)).normalized * speed, ForceMode.Acceleration);
+    }
+
+    void Explode(){
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        if(Vector3.Distance(transform.position, target.transform.position) <= blastRadius){
+            target.GetComponent<CharacterHealth>().TakeDamage(baseDamage / Vector3.Distance(target.transform.position, transform.position));
+        }
+        Destroy(gameObject);
+    }
+
+    void Update(){
+        if(age <= 0){
+            Explode();
+        }
+        rb.AddForce((target.transform.position - transform.position).normalized * speed);
+        age -= Time.deltaTime;
     }
 }
